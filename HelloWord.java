d77889801ddd17e9e75ba9c9f2eab17945df6de5
lixiaoxuan999 <40699377+lixiaoxuan999@users.noreply.github.com>
2018-06-30 01:38:35 +0800

    HelloWord.java测试类
    添加helloword

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+package org.taiji.xxaq.tsgz.model;
+
+/**
+ *  Create by lixiaoxuan on  2018/06/29
+ */
+public class HelloWord {
+
+    //ctrl+J   快速生成main方法
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+
+}
